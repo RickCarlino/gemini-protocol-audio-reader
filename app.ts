52eb452b0a5ec2136fc1e7d667c102b93bef15d2
Rick Carlino <rick.carlino@gmail.com>
2022-11-14 12:59:22 -0600

    Cleanup unused sound files

--- app.ts
+++ app.ts
@@ -1,35 +1,7 @@
-import { parse, Renderer } from "./gemtext";
-import { say } from "./audio";
 import { execSync } from "child_process";
+import { parse } from "./gemtext";
+import { SpeechRenderer } from "./speech-renderer";
 import { sanitizeGeminiURL } from "./url";
-
-const SpeechRenderer: Renderer<void> = {
-  text: (content: string): void => {
-    // Skip carriage returns
-    if (content.length) {
-      say(2, content);
-    }
-  },
-  link: (_url: string, alt: string): void => {
-    say(3, "Link to " + alt);
-  },
-  preformatted: (_content: string[], alt: string): void => {
-    say(4, "Text block " + alt);
-  },
-  heading: (level: number, text: string): void => {
-    say(4 + level, text);
-  },
-  unorderedList: (content: string[]): void => {
-    content.map(text => {
-      say(11, text);
-    });
-  },
-  quote: (content: string): void => {
-    say(10, content);
-  }
-}
-
-// ==== PLAYER / DOWNLOADER:
 
 const rawURL = process.argv.find(text => {
   if (text.includes("gemini:")) {
@@ -41,6 +13,7 @@ if (!rawURL) {
   console.error("Expected a URL starting with `gemini://`");
   process.exit(1);
 }
+
 const url = sanitizeGeminiURL(rawURL);
 
 const gemText = execSync(`npx gemini-fetch ${url}`, { encoding: 'utf-8' });
---
+++ speech-renderer.ts
@@ -0,0 +1,12 @@
+import { say } from "./audio";
+import { Renderer } from "./gemtext";
+
+export const SpeechRenderer: Renderer<void> = {
+    text: (content: string) => (content.length && say(0, content)),
+    link: (_url: string, alt: string) => say(1, "Link to " + alt),
+    preformatted: (_content: string[], alt: string) => say(2, "Text block " + alt),
+    unorderedList: (content: string[]) => content.map(text => say(3, text)),
+    quote: (content: string) => say(4, content),
+    heading: (level: number, text: string) => say(5 + level, text),
+  }
+  
